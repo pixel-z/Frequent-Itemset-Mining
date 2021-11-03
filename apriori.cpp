e47919e6bacb3d1295b8e72e0a6437065d820d06
pixel-z <zishankazi01@gmail.com>
2021-11-03 16:21:04 +0530

    Problemset added

---
+++ apriori.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int minsup;
+    cout<<"Enter the minimum support value: ";
+    cin>>minsup;
+    cout<<"Enter Transactional Dataset filename: ";
+    string input_file;
+    cin>>input_file;
+    cout<<"----------------------------------------------------------------\n";
+
+    // freopen(input_file.c_str(), "r", stdin);
+    freopen("output.txt", "w", stdout);
+    
+    cout<<"raju\n";
+
+
+    return 0;
+}
