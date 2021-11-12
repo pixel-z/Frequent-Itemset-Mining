774c2c96c3e05ad6e173b768a3817de379e5ab23
pixel-z <zishankazi01@gmail.com>
2021-11-12 17:11:51 +0530

    HASH Optimized Apriori done

--- apriori.cpp
+++ apriori.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;  // find_by_order(),order_of_key()
+typedef tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
 
 vector<set<int>> dataset;               // all preprocessed transactions
 vector<vector<int>> Candidates_k;       // potential candidates
 vector<vector<int>> L_k;                // frequent itemsets in kth lvl
-set<int> all_items;                     // all items in whole dataset
-map<int, int> cnt;                      // item count in dataset
 vector<vector<vector<int>>> freq_items; // Final list of items which are frequent
 int minsup;                             // minimum support
 
@@ -34,7 +36,6 @@ vector<vector<int>> candidates_greater_than_minsup()
         if(sup>=minsup)
             new_candidates.push_back(candidate);
     }
-
     return new_candidates;
 }
 
@@ -68,8 +69,8 @@ void next_candidates()
                 v2.push_back(max(c1,c2));
                 Candidates_k.push_back(v2);
             }
-            else
-                break;
+            // else
+            //     break;
         }
     }
     // new lvl potential candidates are found
@@ -112,6 +113,15 @@ void apriori()
         generate_freq_items();
         if(!L_k.empty())
             freq_items.push_back(L_k);
+        // if(L_k[0].size() == 2)
+        // {
+        //     for(auto i : L_k)
+        //     {
+        //         for(auto j : i)
+        //             cout<<j<<" ";
+        //         cout<<endl;
+        //     }
+        // }
     }
 }
 
@@ -172,11 +182,17 @@ void display_frequent_itemsets(vector<vector<vector<int>>> &frequent_itemsets_mi
 
 int main()
 {
+    int type = 1;
+    cout << "Choose the type of optimization strategy:\n";
+    cout << "1. Normal Apriori\n2. Hash Based Technique\n3. Partitioning\n";
+    cout<< "Enter No.: ";
+    cin>>type;
+
     cout << "Enter the minimum support: ";
     cin >> minsup;
     cout << "Enter Transactional Dataset filename: ";
-    string input_file = "SPMF.txt";
-    // cin >> input_file;
+    string input_file = "pfden.dat";
+    cin >> input_file;
     cout << "----------------------------------------------------------------\n";
 
     // cout to output.txt
@@ -224,37 +240,158 @@ int main()
     // ----------------------------------------------------------------------------
     // MAIN code
 
-    // NORMAL APRIORI with no optimization
-    auto start_time = chrono::high_resolution_clock::now();
-    cout << "NORMAL APRIORI:\n";
-    for (int i = 0; i < dataset.size(); i++)
+    if(type == 1)
     {
-        for (auto j : dataset[i])
-        {
-            all_items.insert(j);
-            cnt[j]++;
-        }
-    }
+        // NORMAL APRIORI with no optimization
+        cout << "OUTPUT BY NORMAL APRIORI:\n";
+        set<int> all_items; // all items in whole dataset
+        map<int, int> cnt;  // item count in dataset
 
-    for (auto it : all_items)
-    {
+        auto start_time = chrono::high_resolution_clock::now();
+        for (int i = 0; i < dataset.size(); i++)
+        {
+            for (auto j : dataset[i])
+            {
+                all_items.insert(j);
+                cnt[j]++;
+            }
+        }
+
+        for (auto it : all_items)
+        {
+            if (cnt[it] >= minsup) 
         if (cnt[it] >= minsup) 
-        {
-            // pushing singletons in form of vector
-            vector<int> v = {it};
-            L_k.push_back(v);
+            if (cnt[it] >= minsup) 
+            {
+                // pushing singletons in form of vector
+                vector<int> v = {it};
+                L_k.push_back(v);
+            }
         }
+        freq_items.push_back(L_k);
+
+        apriori();
+
+        display_frequent_itemsets(freq_items);
+        // print_freq_items();
+        auto stop = std::chrono::system_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start_time);
+        cout << "Time taken: " << duration.count() / 1000000.0 << " secs\n";
+        cout << "---------------------------------------------------------------\n";
     }
-    freq_items.push_back(L_k);
+    else if(type == 2)
+    {
+        // HASH APRIORI
+        cout << "OUTPUT BY APRIORI OPTIMIZED BY HASH BASED TECHNIQUES:\n";
+        auto start_time = chrono::high_resolution_clock::now();
 
-    apriori();
+        ordered_set all_items;  // all items in whole dataset
+        map<int, int> cnt;      // item count in dataset
+        for (int i = 0; i < dataset.size(); i++)
+        {
+            for (auto j : dataset[i])
+            {
+                all_items.insert(j);
+                cnt[j]++;
+            }
+        }
 
-    display_frequent_itemsets(freq_items);
-    // print_freq_items();
-    auto stop = std::chrono::system_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start_time);
-    cout << "Time taken: " << duration.count() / 1000000.0 << " secs\n";
-    cout << "---------------------------------------------------------------\n";
+        set<pair<int,int>> bucket[7];
+        int bucket_count[7]={0};
+        for(auto transaction : dataset)
+        {
+            auto itr = transaction.begin();
+
+            while (itr != transaction.end())
+            {
+                auto itr2 = next(itr,1);
+                while (itr2 != transaction.end())
+                {
+                    // all possible pairs in transaction set
+                    int order1 = all_items.order_of_key(*itr);
+                    int order2 = all_items.order_of_key(*itr2);
+
+                    // calculate hash and put into bucket
+                    int hash = (10*order1 + order2)%7;
+                    bucket_count[hash]++;
+                    bucket[hash].insert({*itr,*itr2});
+                    itr2++;
+                }
+                itr++;
+            }
+        }
+
+        // all freq single items are pushed
+        for(auto item : all_items)
+        {
+            if(cnt[item]>=minsup)
+            {
+                vector<int> v = {item};
+                L_k.push_back(v);
+            }
+        }
+        freq_items.push_back(L_k);
+
+        // 2 size itemsets
+        for (int i = 0; i < 7; i++)
+        {
+            if(bucket_count[i]>=minsup)
+            {
+                for (auto p : bucket[i])
+                {
+                    vector<int> v = {p.first,p.second};
+                    Candidates_k.push_back(v);
+                }
+            }
+        }
+        // new lvl potential candidates are found
+        // Pruning
+        set<vector<int>> set_L_k;
+        for(auto i : L_k)
+            set_L_k.insert(i);
+
+        vector<vector<int>> new_candidates;
+        for(auto candidate : Candidates_k)
+        {
+            bool flag = true;   // true = all parents freq
+            vector<int> par = candidate;
+            for (int i = 0; i < candidate.size(); i++)
+            {
+                // candidate = ABCD
+                // removing B and check ACD freq
+                par.erase(par.begin()+i);
+                if(set_L_k.find(par)==set_L_k.end())
+                {
+                    flag = false;
+                    break;
+                }
+                par.insert(par.begin()+i, candidate[i]);
+            }
+            if (flag)
+                new_candidates.push_back(candidate);
+        }
+        Candidates_k = new_candidates;
+
+        Candidates_k=candidates_greater_than_minsup();
+        L_k.clear();
+        for(auto i : Candidates_k)
+            L_k.push_back(i);
+        if(!L_k.empty())
+            freq_items.push_back(L_k);
+        // for(auto i : L_k)
+        // {
+        //     for(auto j : i)
+        //         cout<<j<<" ";
+        //     cout<<endl;
+        // }
+        apriori();
+
+        display_frequent_itemsets(freq_items);
+        auto stop = std::chrono::system_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start_time);
+        cout << "Time taken: " << duration.count() / 1000000.0 << " secs\n";
+        cout << "---------------------------------------------------------------\n";
+    }
 
     return 0;
 }
